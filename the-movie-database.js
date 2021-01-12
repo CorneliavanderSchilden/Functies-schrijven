6ed625682f2eff0dced9c683dd6074271f344218
Cornelia van der Schilden <72788749+CorneliavanderSchilden@users.noreply.github.com>
2021-01-12 11:35:01 +0100

    Add files via upload

---
+++ the-movie-database.js
@@ -0,0 +1,7 @@
+const favMovie = function (title, duratation, stars) {
+    title = "Puff the Magic Dragon";
+    duration = 30;
+    stars = "Puff, Jackie, Living Sneezes";
+    message = title + " lasts for " + duration + " minutes. Stars: " + stars;
+    console.log(message);
+}
